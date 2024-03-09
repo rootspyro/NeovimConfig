44bf974e1762a0644ff0ad14d64216d788a90d33
rootspyro <root.spyro@gmail.com>
2024-03-09 17:53:17 -0400

    Add: Basic Visual Themes and plugins

--- README.md
+++ README.md
@@ -3,6 +3,17 @@ My personal neovim configuration as software developer and a linux user.
 
 ## Installation
 
+### Software required
+
+For the well functionality of this configuration you will required the following extra packages.
+
+- [NodeJS](https://nodejs.org/en)
+- [Gnu Compiler Collection](https://gcc.gnu.org)
+- [Wget](https://www.gnu.org/software/wget/)
+- [Nerd Font](https://github.com/ryanoasis/nerd-fonts#font-installation)
+
+### Installation Steps
+
 1 - Install [Neovim](https://github.com/neovim/neovim/blob/master/INSTALL.md)
 
 2 - Clone the repository into your .config directory
@@ -14,8 +25,17 @@ cd $HOME/.config & git clone https://github.com/rootspyro/NeovimConfig
 
 4 - Lazy.nvim package manager will start to install the plugins.
 
-### Software required
 
-For the well functionality of this configuration you will required the following extra packages.
+### NerdFonts
+I currently using the [HackNerdFont - Regular](https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Regular/HackNerdFont-Regular.ttf).
 
-- [NodeJS](https://nodejs.org/en)
+```sh
+
+# Installation steps
+cd $HOME/.local/share
+
+mkdir fonts & cd fonts
+
+wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Regular/HackNerdFont-Regular.ttf
+```
+
--- init.lua
+++ init.lua
@@ -49,11 +49,64 @@ require("lazy").setup({
   { "folke/neoconf.nvim", cmd = "Neoconf" },
   "folke/neodev.nvim",
 
-    -- THEMES AND VIEW
-    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...}
+  -- THEMES AND VIEW
+  { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
 
+  "nvim-treesitter/nvim-treesitter",
+
+  "ryanoasis/vim-devicons",
+
+  "nvim-lualine/lualine.nvim",
+
+  "nvim-tree/nvim-web-devicons",
 })
 
 vim.cmd('colorscheme gruvbox')
 
 vim.g.mapleader = ' '
+
+-- CONFIGURATION SECTION
+
+
+-- LUALINE
+
+require('lualine').setup {
+    options = {
+      icons_enabled = true,
+      theme = 'gruvbox',
+      component_separators = { left = '', right = ''},
+      section_separators = { left = '', right = ''},
+      disabled_filetypes = {
+        statusline = {},
+        winbar = {},
+      },
+      ignore_focus = {},
+      always_divide_middle = true,
+      globalstatus = false,
+      refresh = {
+        statusline = 1000,
+        tabline = 1000,
+        winbar = 1000,
+      }
+    },
+    sections = {
+      lualine_a = {'mode'},
+      lualine_b = {'branch', 'diff', 'diagnostics'},
+      lualine_c = {'filename'},
+      lualine_x = {'encoding', 'fileformat', 'filetype'},
+      lualine_y = {'progress'},
+      lualine_z = {'location'}
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = {'filename'},
+      lualine_x = {'location'},
+      lualine_y = {},
+      lualine_z = {}
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {}
+  }
